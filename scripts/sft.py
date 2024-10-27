d61cd7f4c972dff14b5f7e96e3b9093c799cc5ac
Sergei Bratchikov <hivaze.me@gmail.com>
2024-10-27 16:48:31 +0000

    Updated .lock and added qwen to liger kernels

--- scripts/sft.py
+++ scripts/sft.py
@@ -85,7 +85,7 @@ def main():
         attn_implementation=model_config.attn_implementation
     )
     if sft_config.use_liger:
-        from liger_kernel.transformers import apply_liger_kernel_to_llama, apply_liger_kernel_to_mistral
+        from liger_kernel.transformers import apply_liger_kernel_to_llama, apply_liger_kernel_to_mistral, apply_liger_kernel_to_qwen2
         apply_liger_kernel_to_llama(
             rope=False,
             swiglu=True,
@@ -94,6 +94,13 @@ def main():
             rms_norm=False
         )
         apply_liger_kernel_to_mistral(
+            rope=False,
+            swiglu=True,
+            cross_entropy=False,
+            fused_linear_cross_entropy=True,
+            rms_norm=False
+        )
+        apply_liger_kernel_to_qwen2(
             rope=False,
             swiglu=True,
             cross_entropy=False,
